be394a9305e1fea4d1633ef8072878d922f80997
RatiboR1978 <klov29@mail.ru>
2017-10-19 04:34:38 +0700

    Сделал кнопку вверх прописал везде ховеры

--- js/common.js
+++ js/common.js
@@ -8,6 +8,18 @@ $(function () {
     $('#close').on('click', function () {
         $('.panel__wrap').fadeOut(350);
         $('.panel__trigger').delay(350).animate({height:'toggle', padding:'toggle'}, 350);
-    })
+    });
+
+    $(window).on('scroll',function(){
+        if ($(this).scrollTop() > 2000) {
+            $('.arrowUp__up').css('display', 'block');
+        } else if ($(this).scrollTop() < 2000) {
+            $('.arrowUp__up').css('display', 'none');
+        }
+    });
+
+    $('.arrowUp__up').on('click', function () {
+        $('body,html').animate({scrollTop:0},800);
+    });
 
 });
