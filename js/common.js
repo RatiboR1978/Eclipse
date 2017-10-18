fcdb3132dc1934bd9dfbdddd5ea24cc104243653
RatiboR1978 <klov29@mail.ru>
2017-10-18 08:16:58 +0700

    Сделал видвигающуюся панель

--- js/common.js
+++ js/common.js
@@ -1,8 +1,13 @@
 $(function () {
- 
+    $('.panel__trigger').on('click', function () {
+        $(this).animate({height:'toggle', padding:'toggle'}, 350);
+        $('.panel__wrap').delay(350).fadeIn();
+        return false;
+    });
 
-
-
-
+    $('#close').on('click', function () {
+        $('.panel__wrap').fadeOut(350);
+        $('.panel__trigger').delay(350).animate({height:'toggle', padding:'toggle'}, 350);
+    })
 
 });
